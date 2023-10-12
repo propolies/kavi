d4033341144ded300b99e2e221a7fa992fb135d9
propolies <propolies@gmail.com>
2023-10-12 14:24:01 +0200

    add nestedRouter

--- src/handle.ts
+++ src/handle.ts
@@ -1,20 +1,21 @@
 import type { Handle } from "@sveltejs/kit"
 import { middleware } from "./middleware"
 
-export const createHandle = <Router extends Object>(router: Router): Handle => {
+export const createHandle = (router: any): Handle => {
   return async ({ event, resolve }) => {
     const { searchParams, pathname } = event.url
     const api = searchParams.get('api')
   
     if (pathname.startsWith('/') && api) {
-      if (api === "getRouterKeys") return new Response(JSON.stringify(Object.keys(router)))
+      if (api === "getRouterKeys") {
+        const keys = getDeepKeys(router)
+        return new Response(JSON.stringify(keys))
+      }
       
       middleware.event = event
-      let reqBody
-      reqBody = await event.request.json().catch(() => '')
-      // @ts-ignore
-      const body = await router[api as keyof typeof router](reqBody)
-  
+      let reqBody = await event.request.json().catch(() => '')
+      
+      const body = await nestedCall(api.split("."), router, reqBody)
       const { status, statusText, headers: responseHeaders } = await resolve(event)
   
       const headers = {
@@ -29,4 +30,26 @@ export const createHandle = <Router extends Object>(router: Router): Handle => {
   
     return await resolve(event)
   }
+}
+
+const nestedCall = async (path: string[], router: (...args: unknown[]) => unknown | object, params: unknown): Promise<unknown> => {
+  const [route, ...rest] = path
+  router = router[route as keyof typeof router]
+  if (!rest.length) return router(params)
+  return nestedCall(rest, router, params)
+}
+
+type NestedObj = {[key: string]: NestedObj | unknown}
+const getDeepKeys = (obj: object) => {
+  let res: NestedObj = {}
+
+  Object.entries(obj).forEach(([ key, value ]) => {
+    if (typeof value == "object") {
+      res[key] = getDeepKeys(value)
+    } else {
+      res[key] = ""
+    }
+  })
+  console.log(res)
+  return res
 }
